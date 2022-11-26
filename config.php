bbe3133de2de9ceee7bad34424e0c8e91617fac8
GuilhermeLupi <guilhermelupiesilva@gmail.com>
2022-11-26 15:22:12 -0300

    first

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+    $dbHost = 'Localhost';
+    $dbUsername = 'root';
+    $dbPassword = 'drew123';
+    $dbName = 'midnight-japan';
+
+    $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+
+?>
